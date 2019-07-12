e9d770f19790cc73707c36d2824f81d9ef7bcf48
fakersaber <785702978@qq.com>
2019-07-12 22:18:00 +0800

    Create Grid and UI

---
+++ Assets/Hex_Map/HexGrid.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+    public HexCell cellPrefab;
+    public Text cellLabelPrefab;
+
+
+
+    private HexCell[] cells;
+    private Canvas gridCanvas;
+    //减少栈GC，描述Cell and lable position
+    private Vector3 position;
+    private Vector2 anchoredPos;
+
+    private void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>(); 
+
+        //y轴不变
+        position.y = 0f;
+        cells = new HexCell[height * width];
+        int index = 0;
+        for(int z = 0; z < height; ++z)
+        {
+            for(int x = 0; x<width; ++x)
+            {
+                cells[index] = Instantiate<HexCell>(cellPrefab);
+                position.x = x * 10f;
+                position.z = z * 10f;
+                //设置父系坐标，可以不关心在world space下的位置
+                cells[index].transform.SetParent(this.transform);
+                cells[index].transform.localPosition = position;
+
+                Text label = Instantiate<Text>(cellLabelPrefab);
+                anchoredPos.x = x * 10f;
+                anchoredPos.y = z * 10f;
+                label.rectTransform.SetParent(gridCanvas.transform, false);
+                label.rectTransform.anchoredPosition = anchoredPos;
+                label.text = x.ToString() + "\n" + z.ToString();
+            }
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Hex_Map/HexMetrics.cs
+++ Assets/Hex_Map/HexMetrics.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public static class HexMetrics
 {
+    public const float outerRadius = 10f;
+    public const float innerRadius = outerRadius * 0.8660254f;
+
+    //pointy side up of orientate a hexagon
+    public static Vector3[] Edges =
+    {
+        new Vector3(0f, 0f,outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius ,0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius ,0f, 0.5f * outerRadius)
+    };
 
 }
