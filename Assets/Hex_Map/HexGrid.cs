f59f1eb7d4523be4c800bb0315862626d8e02208
fakersaber <785702978@qq.com>
2019-07-13 16:12:34 +0800

    Hexagonal created

--- Assets/Hex_Map/HexGrid.cs
+++ Assets/Hex_Map/HexGrid.cs
@@ -11,41 +11,50 @@ public class HexGrid : MonoBehaviour
     public Text cellLabelPrefab;
 
 
-
+    private HexMesh hexMesh;
     private HexCell[] cells;
     private Canvas gridCanvas;
+    
+
     //减少栈GC，描述Cell and lable position
     private Vector3 position;
     private Vector2 anchoredPos;
 
     private void Awake()
     {
+        hexMesh = GetComponentInChildren<HexMesh>();
         gridCanvas = GetComponentInChildren<Canvas>(); 
 
         //y轴不变
         position.y = 0f;
         cells = new HexCell[height * width];
         int index = 0;
-        for(int z = 0; z < height; ++z)
+        for (uint z = 0; z < height; ++z)
         {
-            for(int x = 0; x<width; ++x)
+            for(uint x = 0; x<width; ++x)
             {
                 cells[index] = Instantiate<HexCell>(cellPrefab);
-                position.x = x * 10f;
-                position.z = z * 10f;
+                //z为偶数情况： position.x = HexMetrics.innerRadius + x * 2f* HexMetrics.innerRadius= (x + 0.5f)* 2f* HexMetrics.innerRadius;
+                //z为奇数情况： position.x = x * 2f* HexMetrics.innerRadius;
+                //融合上式： z/2当z为偶数时与z*0.5f相同
+
+
+                position.x = (z & 0x1ul) * HexMetrics.innerRadius + x * 2f * HexMetrics.innerRadius;
+                position.z = z * 1.5f * HexMetrics.outerRadius;
                 //如果原本的transform有父类transform，那么第二个param为ture时会保证更换父类后原物体的position在world space下保持不变
                 //在此处两个地方都是需要跟随父系一起变换，所以直接设置为false
                 cells[index].transform.SetParent(this.transform,false);
                 cells[index].transform.localPosition = position;
 
                 Text label = Instantiate<Text>(cellLabelPrefab);
-                anchoredPos.x = x * 10f;
-                anchoredPos.y = z * 10f;
+                anchoredPos.x = position.x;
+                anchoredPos.y = position.z;
                 //不要又变回原位置，跟随父级一起变
                 label.rectTransform.SetParent(gridCanvas.transform,false);
                 label.rectTransform.anchoredPosition = anchoredPos;
                 
                 label.text = x.ToString() + "\n" + z.ToString();
+                index++;
             }
         }
     }
@@ -53,9 +62,9 @@ public class HexGrid : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        hexMesh.Triangulate(cells);
     }
 
     // Update is called once per frame
---
+++ Assets/Hex_Map/HexMesh.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour
+{
+    Mesh hexMesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+
+
+    private void Awake()
+    {
+        //手动创建mesh
+        GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+        hexMesh.name = "Hex Mesh";
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+    }
+
+
+    public void Triangulate(HexCell[] cells)
+    {
+        hexMesh.Clear();
+        vertices.Clear();
+        triangles.Clear();
+        for(int i = 0; i < cells.Length; ++i)
+        {
+            Triangulate(cells[i]);
+        }
+        hexMesh.vertices = this.vertices.ToArray();
+        hexMesh.triangles = this.triangles.ToArray();
+        hexMesh.RecalculateNormals();
+    }
+
+    private void Triangulate(HexCell cell)
+    {
+        //根据cell的中心位置生成六边形
+
+        for(int i = 0; i < 6; ++i)
+        {
+            //注意可能最后一条边越界，可以除余或者直接再加一条边
+            int vertexIndex = vertices.Count;
+            vertices.Add(cell.transform.localPosition);
+            vertices.Add(cell.transform.localPosition + HexMetrics.Edges[i]);
+            vertices.Add(cell.transform.localPosition + HexMetrics.Edges[i+1]);
+
+            //对应顶点索引,这里公用顶点暂未优化
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
+        }
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
